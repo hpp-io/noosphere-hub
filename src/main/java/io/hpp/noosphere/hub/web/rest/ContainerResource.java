c2dd67cccba7ddd74eb24ba9bb73722a44fc9d06
jungsoo827 <jungsoo.home@gmail.com>
2025-10-09 14:54:09 +1100

    HPPS-54 updated to display container detailed info for search result

--- src/main/java/io/hpp/noosphere/hub/web/rest/ContainerResource.java
+++ src/main/java/io/hpp/noosphere/hub/web/rest/ContainerResource.java
@@ -55,159 +55,158 @@ import tech.jhipster.web.util.ResponseUtil;
 @Tag(name = "Container", description = "Container Controller")
 public class ContainerResource {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ContainerResource.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ContainerResource.class);
 
-  private static final String ENTITY_NAME = "nooSphereHubContainer";
-  private final IAuthenticationFacade authenticationFacade;
-  private final ContainerService containerService;
-  private final ContainerRepository containerRepository;
+    private static final String ENTITY_NAME = "nooSphereHubContainer";
+    private final IAuthenticationFacade authenticationFacade;
+    private final ContainerService containerService;
+    private final ContainerRepository containerRepository;
 
-  private final UserService userService;
+    private final UserService userService;
 
-  @Value("${jhipster.clientApp.name}")
-  private String applicationName;
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
 
-  public ContainerResource(
-    ContainerService containerService,
-    ContainerRepository containerRepository,
-    UserService userService,
-    IAuthenticationFacade authenticationFacade
-  ) {
-    this.containerService = containerService;
-    this.containerRepository = containerRepository;
-    this.authenticationFacade = authenticationFacade;
-    this.userService = userService;
-  }
-
-  /**
-   * {@code POST  /containers} : Create a new container.
-   *
-   * @param containerDTO the containerDTO to create.
-   * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new containerDTO, or with status {@code 400 (Bad Request)} if the
-   * container has already an ID.
-   * @throws URISyntaxException if the Location URI syntax is incorrect.
-   */
-  @PostMapping("")
-  @JsonView(JsonViewType.Update.class)
-  public ResponseEntity<ContainerDTO> createContainer(@RequestBody ContainerDTO containerDTO) throws URISyntaxException {
-    LOG.debug("REST request to save Container : {}", containerDTO);
-    if (containerDTO.getId() != null) {
-      throw new BadRequestAlertException("A new container cannot already have an ID", ENTITY_NAME, "idexists");
-    }
-    Instant now = Instant.now();
-    containerDTO = containerService.create(authenticationFacade.getUserId(), containerDTO, now);
-    return ResponseEntity.created(new URI("/api/containers/" + containerDTO.getId()))
-      .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, containerDTO.getId().toString()))
-      .body(containerDTO);
-  }
-
-  /**
-   * {@code PUT  /containers/:id} : Updates an existing container.
-   *
-   * @param id           the id of the containerDTO to save.
-   * @param containerDTO the containerDTO to update.
-   * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated containerDTO, or with status {@code 400 (Bad Request)} if the
-   * containerDTO is not valid, or with status {@code 500 (Internal Server Error)} if the containerDTO couldn't be updated.
-   * @throws URISyntaxException if the Location URI syntax is incorrect.
-   */
-  @PutMapping("/{id}")
-  @JsonView(JsonViewType.Update.class)
-  public ResponseEntity<ContainerDTO> updateContainer(
-    @PathVariable(value = "id", required = true) final UUID id,
-    @Valid @RequestBody ContainerDTO containerDTO
-  ) throws PermissionDeniedException {
-    LOG.debug("REST request to update Container : {}, {}", id, containerDTO);
-    if (containerDTO.getId() == null) {
-      throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-    }
-    if (!Objects.equals(id, containerDTO.getId())) {
-      throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+    public ContainerResource(
+        ContainerService containerService,
+        ContainerRepository containerRepository,
+        UserService userService,
+        IAuthenticationFacade authenticationFacade
+    ) {
+        this.containerService = containerService;
+        this.containerRepository = containerRepository;
+        this.authenticationFacade = authenticationFacade;
+        this.userService = userService;
     }
 
-    if (!containerRepository.existsById(id)) {
-      throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+    /**
+     * {@code POST  /containers} : Create a new container.
+     *
+     * @param containerDTO the containerDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new containerDTO, or with status {@code 400 (Bad Request)} if the
+     * container has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("")
+    @JsonView(JsonViewType.Update.class)
+    public ResponseEntity<ContainerDTO> createContainer(@RequestBody ContainerDTO containerDTO) throws URISyntaxException {
+        LOG.debug("REST request to save Container : {}", containerDTO);
+        if (containerDTO.getId() != null) {
+            throw new BadRequestAlertException("A new container cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        Instant now = Instant.now();
+        containerDTO = containerService.create(authenticationFacade.getUserId(), containerDTO, now);
+        return ResponseEntity.created(new URI("/api/containers/" + containerDTO.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, containerDTO.getId().toString()))
+            .body(containerDTO);
     }
-    Instant now = Instant.now();
-    containerDTO = containerService.partialUpdate(userService, authenticationFacade.getUserId(), containerDTO, now);
-    return ResponseEntity.ok()
-      .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, containerDTO.getId().toString()))
-      .body(containerDTO);
-  }
 
+    /**
+     * {@code PUT  /containers/:id} : Updates an existing container.
+     *
+     * @param id           the id of the containerDTO to save.
+     * @param containerDTO the containerDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated containerDTO, or with status {@code 400 (Bad Request)} if the
+     * containerDTO is not valid, or with status {@code 500 (Internal Server Error)} if the containerDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PutMapping("/{id}")
+    @JsonView(JsonViewType.Update.class)
+    public ResponseEntity<ContainerDTO> updateContainer(
+        @PathVariable(value = "id", required = true) final UUID id,
+        @Valid @RequestBody ContainerDTO containerDTO
+    ) throws PermissionDeniedException {
+        LOG.debug("REST request to update Container : {}, {}", id, containerDTO);
+        if (containerDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, containerDTO.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
 
-  /**
-   * {@code POST  /containers/search} : search containers.
-   *
-   * @param searchVm the search criteria of the request.
-   * @param pageable the pagination information.
-   * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of containers in body.
-   */
-  @PostMapping("/search")
-  @Operation(summary = "Search Container")
-  @io.swagger.v3.oas.annotations.parameters.RequestBody(
-    required = true,
-    description = "Search Criteria",
-    content = @Content(schema = @Schema(implementation = SearchContainerVm.class), mediaType = "application/json")
-  )
-  @ApiResponses(
-    {
-      @ApiResponse(
-        responseCode = "200",
-        content = @Content(
-          mediaType = MediaType.APPLICATION_JSON_UTF8_VALUE,
-          array = @ArraySchema(schema = @Schema(implementation = ContainerDTO.class))
-        ),
-        description = "Successful operation"
-      ),
-      @ApiResponse(
-        responseCode = "500",
-        content = @Content(mediaType = MediaType.APPLICATION_JSON_UTF8_VALUE),
-        description = "Internal server error"
-      ),
+        if (!containerRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+        Instant now = Instant.now();
+        containerDTO = containerService.partialUpdate(userService, authenticationFacade.getUserId(), containerDTO, now);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, containerDTO.getId().toString()))
+            .body(containerDTO);
     }
-  )
-  @JsonView(JsonViewType.Shallow.class)
-  public ResponseEntity<List<ContainerDTO>> search(
-    @RequestBody SearchContainerVm searchVm,
-    @org.springdoc.core.annotations.ParameterObject Pageable pageable
-  ) {
-    LOG.debug("REST request to search Containers");
-    Page<ContainerDTO> page = containerService.search(
-      searchVm.getName(),
-      searchVm.getStatusCode(),
-      searchVm.getCreatedByUserId(),
-      pageable
-    );
-    HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
-    return ResponseEntity.ok().headers(headers).body(page.getContent());
-  }
 
-  /**
-   * {@code GET  /containers/:id} : get the "id" container.
-   *
-   * @param id the id of the containerDTO to retrieve.
-   * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the containerDTO, or with status {@code 404 (Not Found)}.
-   */
-  @GetMapping("/{id}")
-  @JsonView(JsonViewType.Update.class)
-  public ResponseEntity<ContainerDTO> getContainer(@PathVariable("id") UUID id) {
-    LOG.debug("REST request to get Container : {}", id);
-    Optional<ContainerDTO> containerDTO = containerService.findOne(id);
-    return ResponseUtil.wrapOrNotFound(containerDTO);
-  }
+    /**
+     * {@code POST  /containers/search} : search containers.
+     *
+     * @param searchVm the search criteria of the request.
+     * @param pageable the pagination information.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of containers in body.
+     */
+    @PostMapping("/search")
+    @Operation(summary = "Search Container")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        required = true,
+        description = "Search Criteria",
+        content = @Content(schema = @Schema(implementation = SearchContainerVm.class), mediaType = "application/json")
+    )
+    @ApiResponses(
+        {
+            @ApiResponse(
+                responseCode = "200",
+                content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_UTF8_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = ContainerDTO.class))
+                ),
+                description = "Successful operation"
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                content = @Content(mediaType = MediaType.APPLICATION_JSON_UTF8_VALUE),
+                description = "Internal server error"
+            ),
+        }
+    )
+    @JsonView(JsonViewType.Update.class)
+    public ResponseEntity<List<ContainerDTO>> search(
+        @RequestBody SearchContainerVm searchVm,
+        @org.springdoc.core.annotations.ParameterObject Pageable pageable
+    ) {
+        LOG.debug("REST request to search Containers");
+        Page<ContainerDTO> page = containerService.search(
+            searchVm.getName(),
+            searchVm.getStatusCode(),
+            searchVm.getCreatedByUserId(),
+            pageable
+        );
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
 
-  /**
-   * {@code DELETE  /containers/:id} : delete the "id" container.
-   *
-   * @param id the id of the containerDTO to delete.
-   * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-   */
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> deleteContainer(@PathVariable("id") UUID id) throws PermissionDeniedException  {
-    LOG.debug("REST request to delete Container : {}", id);
-    containerService.delete(authenticationFacade.getUserId(), id);
-    return ResponseEntity.noContent()
-      .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
-      .build();
-  }
+    /**
+     * {@code GET  /containers/:id} : get the "id" container.
+     *
+     * @param id the id of the containerDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the containerDTO, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/{id}")
+    @JsonView(JsonViewType.Update.class)
+    public ResponseEntity<ContainerDTO> getContainer(@PathVariable("id") UUID id) {
+        LOG.debug("REST request to get Container : {}", id);
+        Optional<ContainerDTO> containerDTO = containerService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(containerDTO);
+    }
+
+    /**
+     * {@code DELETE  /containers/:id} : delete the "id" container.
+     *
+     * @param id the id of the containerDTO to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteContainer(@PathVariable("id") UUID id) throws PermissionDeniedException {
+        LOG.debug("REST request to delete Container : {}", id);
+        containerService.delete(authenticationFacade.getUserId(), id);
+        return ResponseEntity.noContent()
+            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+            .build();
+    }
 }
