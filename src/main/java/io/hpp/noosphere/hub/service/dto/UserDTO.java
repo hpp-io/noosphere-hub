dc040e7e9bb4dc9729bf86c652da897a40744902
jungsoo827 <jungsoo.home@gmail.com>
2025-11-03 18:16:38 +1100

    #22 added update user profile feature

--- src/main/java/io/hpp/noosphere/hub/service/dto/UserDTO.java
+++ src/main/java/io/hpp/noosphere/hub/service/dto/UserDTO.java
@@ -23,45 +23,56 @@ import lombok.NoArgsConstructor;
 @Schema
 public class UserDTO implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  @JsonView(JsonViewType.Shallow.class)
-  private String id;
-  @JsonView(JsonViewType.Shallow.class)
-  private String name;
-  @JsonView(JsonViewType.Full.class)
-  private String firstName;
-  @JsonView(JsonViewType.Full.class)
-  private String lastName;
-  @JsonView(JsonViewType.Full.class)
-  private String login;
-  @JsonView(JsonViewType.Shallow.class)
-  private String email;
-  @JsonView(JsonViewType.Full.class)
-  private String imageUrl;
-  @JsonView(JsonViewType.Full.class)
-  private String apiKey;
-  @JsonView(JsonViewType.Full.class)
-  private String walletAddress;
-  @JsonView(JsonViewType.Shallow.class)
-  private String langKey;
-  @JsonView(JsonViewType.Full.class)
-  private Boolean activated;
-  @JsonView(JsonViewType.Full.class)
-  private Set<String> authorities = new HashSet<>();
+    @JsonView(JsonViewType.Shallow.class)
+    private String id;
 
-  public UserDTO(User user) {
-    this.id = user.getId();
-    this.login = user.getLogin();
-    this.name = user.getName();
-    this.firstName = user.getFirstName();
-    this.lastName = user.getLastName();
-    this.email = user.getEmail();
-    this.imageUrl = user.getImageUrl();
-    this.apiKey = user.getApiKey();
-    this.walletAddress = user.getWalletAddress();
-    this.langKey = user.getLangKey();
-    this.activated = user.isActivated();
-    this.authorities = user.getAuthorities().stream().map(Authority::getName).collect(Collectors.toSet());
-  }
+    @JsonView(JsonViewType.Shallow.class)
+    private String name;
+
+    @JsonView(JsonViewType.Update.class)
+    private String firstName;
+
+    @JsonView(JsonViewType.Update.class)
+    private String lastName;
+
+    @JsonView(JsonViewType.Full.class)
+    private String login;
+
+    @JsonView(JsonViewType.Shallow.class)
+    private String email;
+
+    @JsonView(JsonViewType.Update.class)
+    private String imageUrl;
+
+    @JsonView(JsonViewType.Update.class)
+    private String apiKey;
+
+    @JsonView(JsonViewType.Update.class)
+    private String walletAddress;
+
+    @JsonView(JsonViewType.Shallow.class)
+    private String langKey;
+
+    @JsonView(JsonViewType.Update.class)
+    private Boolean activated;
+
+    @JsonView(JsonViewType.Full.class)
+    private Set<String> authorities = new HashSet<>();
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.login = user.getLogin();
+        this.name = user.getName();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.imageUrl = user.getImageUrl();
+        this.apiKey = user.getApiKey();
+        this.walletAddress = user.getWalletAddress();
+        this.langKey = user.getLangKey();
+        this.activated = user.isActivated();
+        this.authorities = user.getAuthorities().stream().map(Authority::getName).collect(Collectors.toSet());
+    }
 }
