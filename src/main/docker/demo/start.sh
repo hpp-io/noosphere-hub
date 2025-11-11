738f0477026aeaa9f485b2cfb799f0f7f5b72a86
jungsoo827 <jungsoo.home@gmail.com>
2025-11-11 11:34:59 +1100

    updated demo environment docker files

--- src/main/docker/demo/start.sh
+++ src/main/docker/demo/start.sh
@@ -1,5 +1,6 @@
 docker-compose -f ./services.yml up noosphere-mysql -d
 docker-compose -f ./services.yml up noosphere-noosphere-consul-config-loader -d
+sleep 40
 docker-compose -f ./services.yml up noosphere-auth -d
 docker-compose -f ./services.yml up noosphere-consul -d
 docker-compose -f ./services.yml up noosphere-pulsar -d
