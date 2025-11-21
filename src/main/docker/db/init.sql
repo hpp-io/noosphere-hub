9590afd7423e2edb7d62ac7591b0a266c1467ffe
jjang <yoonjoo.jang@gmail.com>
2025-11-21 17:02:43 +0900

    Add scheduler database initialization script and permissions

--- src/main/docker/db/init.sql
+++ src/main/docker/db/init.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE nshub;
 CREATE DATABASE keycloak;
+CREATE DATABASE scheduler;
 GRANT ALL PRIVILEGES ON keycloak.* TO 'nsuser';
 GRANT ALL PRIVILEGES ON nshub.* TO 'nsuser';
+GRANT ALL PRIVILEGES ON nshub.* TO 'scheduler';
